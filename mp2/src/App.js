397aba683a7a9327b5053765dbda5a579a4e9980
unknown <trishadennise.uy.iics@ust.edu.ph>
2022-03-28 16:34:30 +0800

    first commit

---
+++ mp2/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import React, { useState } from "react";
+import SingleCard from "./SingleCard";
+
+const cardImages = [];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
+  return (
+    <div className="App">
+      <h1>Card Matching</h1>
+      <button onClick={shuffleCards}>Start!</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
